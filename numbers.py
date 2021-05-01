9084a0251d319e559c78f6f533aaa953d39b85eb
aa-ag <aaron.aguerrevere@gmail.com>
2021-05-01 07:20:59 -0500

    added numbers.py and base code

---
+++ numbers.py
@@ -0,0 +1,19 @@
+#########--------- IMPORTS ---------#########
+from bs4 import BeautifulSoup
+import requests
+
+
+#########--------- FUNCTIONS ---------#########
+def numbers_types():
+    '''
+      using `requests`, fetch url provided
+      then create BeautifulSoup object with
+      parsed data, count lenth of list created by `find_all()`
+      grabbing class and finally, create txt with entire list
+    '''
+    pass
+
+
+#########--------- DRIVER CODE ---------#########
+if __name__ == "__main__":
+    numbers_types()
