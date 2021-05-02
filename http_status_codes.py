1baa9cd72b33607356971255aa613bcd5820d299
aa-ag <aaron.aguerrevere@gmail.com>
2021-05-02 12:50:42 -0500

    edited/fixed all error for status everywhere in program, includeing program name

--- http_status_codes.py
+++ http_status_codes.py
@@ -1,16 +1,16 @@
 #########--------- IMPORTS ---------#########
 from bs4 import BeautifulSoup
 import requests
-import csv
 
 
 #########--------- FUNCTIONS ---------#########
-def get_http_errors():
+def get_http_status_codes():
     '''
       using `requests`, fetch url provided
       then create BeautifulSoup object with
       parsed data, count lenth of list created by `find_all()`
-      and finally, create csv with entire list
+      and finally, create txt doc with all status codes and
+      their descriptions
     '''
 
     # grab info from source
@@ -21,19 +21,20 @@ def get_http_errors():
     soup = BeautifulSoup(page.content, 'html.parser')
 
     # grab titles and descriptions to create dict with both
-    http_errors_titles = [i.text.lstrip(' ') for i in soup.find_all("dt")]
+    http_status_titles = [i.text.lstrip(' ') for i in soup.find_all("dt")]
 
-    http_errors_descriptions = [j.find_next(
+    http_status_descriptions = [j.find_next(
         'dd').text for j in soup.find_all("dt")]
 
-    all_http_errors = dict(zip(http_errors_titles, http_errors_descriptions))
+    all_http_status = dict(
+        zip(http_status_titles, http_status_descriptions))
 
-    f = open('http_errors.txt', 'w')
+    f = open('http_status.txt', 'w')
 
-    for error, description in all_http_errors.items():
-        f.write(f"{error} - {description}\n")
+    for status, description in all_http_status.items():
+        f.write(f"{status} - {description}\n")
 
 
 #########--------- DRIVER CODE ---------#########
 if __name__ == "__main__":
-    get_http_errors()
+    get_http_status_codes()
