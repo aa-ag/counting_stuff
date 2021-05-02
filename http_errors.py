40f0e685b3f43d17fb578290696310e3428ab618
aa-ag <aaron.aguerrevere@gmail.com>
2021-05-02 12:04:46 -0500

    creating .txt file with result from all_http_errors_titles

--- http_errors.py
+++ http_errors.py
@@ -5,7 +5,7 @@ import csv
 
 
 #########--------- FUNCTIONS ---------#########
-def list_http_errors():
+def get_http_errors():
     '''
       using `requests`, fetch url provided
       then create BeautifulSoup object with
@@ -13,25 +13,26 @@ def list_http_errors():
       and finally, create csv with entire list
     '''
 
+    # grab info from source
     url = 'https://en.wikipedia.org/wiki/List_of_HTTP_status_codes'
 
     page = requests.get(url)
 
     soup = BeautifulSoup(page.content, 'html.parser')
 
+    # grab titles and descriptions to create dict with both
     all_http_errors_titles = [i.text.lstrip(' ') for i in soup.find_all("dt")]
 
-    all_http_errors_descriptions = [j.text for j in soup.find_all("dd")]
+    # print(len(all_http_errors_titles)) # 97
 
-    all_http_errors = dict()
+    # all_http_errors_decriptions = [j.text for j in soup.find_all("dd")]
 
-    for x in all_http_errors_titles:
-        for y in all_http_errors_descriptions:
-            all_http_errors[x] = y
+    f = open('http_errors.txt', 'w')
 
-    print(all_http_errors)
+    for error in all_http_errors_titles:
+        f.write(error + '\n')
 
 
 #########--------- DRIVER CODE ---------#########
 if __name__ == "__main__":
-    list_http_errors()
+    get_http_errors()
