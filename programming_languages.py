ac9737d90284d0744bcf12ccfc400e2e4a40c813
aa-ag <aaron.aguerrevere@gmail.com>
2021-05-08 15:11:48 -0500

    added lis, grabbed all line items from soup object in programming_languages.py

--- programming_languages.py
+++ programming_languages.py
@@ -5,13 +5,15 @@ import requests
 
 #########--------- FUNCTIONS ---------#########
 def count_programming_languages():
-    url = 'https://www.wikiwand.com/en/List_of_programming_languages'
+    url = 'https://en.wikipedia.org/wiki/List_of_programming_languages'
 
     page = requests.get(url)
 
     soup = BeautifulSoup(page.content, 'html.parser')
 
-    print(soup)
+    lis = [li for li in soup.find_all("li")]
+
+    print(lis)
 
 
 #########--------- DRIVER CODE ---------#########
